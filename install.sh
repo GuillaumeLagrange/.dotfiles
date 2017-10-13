252fd0500f77392ba38095ac925a5f150afe928a
Guillaume Lagrange <guillaume.lagrange@telecom-paristech.fr>
2017-10-13 17:01:16 +0200

    Slight change

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 cd ~
 rm -rf .vim .vimrc .zshrc .config/i3 .config/i3status
-ln -s .dotfiles/.vimrc .vimrc
-ln -s .dotfiles/.zshrc .zshrc
-ln -s .dotfiles/.config/i3 .config/i3
-ln -s .dotfiles/.config/i3status .config/i3status
+ln -s ~/.dotfiles/.vimrc .vimrc
+ln -s ~/.dotfiles/.zshrc .zshrc
+ln -s ~/.dotfiles/.config/i3 ~/.config/i3
+ln -s ~/.dotfiles/.config/i3status ~/.config/i3status
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
