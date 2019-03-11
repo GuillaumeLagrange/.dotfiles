92073589f37a4edb9b4a7d4eee91b463d381258c
Guillaume Lagrange <guillaume.lagrange@telecom-paristech.fr>
2019-03-11 23:07:02 +0100

    Clean up i3 config to allow local config

---
+++ generate_i3_config.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+cat i3/config_common i3/config_local > ~/.config/i3/config
--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
+./generate_i3_config.sh
 cd ~
-rm -rf .vim .vimrc .zshrc .config/i3 .config/i3status
+rm -rf .vim .vimrc .zshrc .config/i3status
 ln -s ~/.dotfiles/.vimrc .vimrc
 ln -s ~/.dotfiles/.zshrc .zshrc
-ln -s ~/.dotfiles/.config/i3 ~/.config/i3
+ln -s ~/.dotfiles/i3status ~/.config/i3status
 ln -s ~/.dotfiles/.config/i3status ~/.config/i3status
 ln -s ~/.dotfiles/scripts scripts
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
