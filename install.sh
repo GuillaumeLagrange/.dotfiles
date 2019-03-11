97731d0f20be1773039b5bcc3b07d6843cf69cf9
Guillaume Lagrange <guillaume.lagrange@telecom-paristech.fr>
2019-03-11 23:16:45 +0100

    Move i3status files

--- install.sh
+++ install.sh
@@ -4,7 +4,6 @@ rm -rf .vim .vimrc .zshrc .config/i3status
 ln -s ~/.dotfiles/.vimrc .vimrc
 ln -s ~/.dotfiles/.zshrc .zshrc
 ln -s ~/.dotfiles/i3status ~/.config/i3status
-ln -s ~/.dotfiles/.config/i3status ~/.config/i3status
 ln -s ~/.dotfiles/scripts scripts
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
