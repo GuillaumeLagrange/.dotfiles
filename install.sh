73c1d4d5efa6a849b59f1be8cc432362fcafaaea
Guillaume Lagrange <guillaume.lagrange@telecom-paristech.fr>
2019-03-11 23:19:45 +0100

    Cleanup install script

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 ./generate_i3_config.sh
 cd ~
-rm -rf .vim .vimrc .zshrc .config/i3status
-ln -s ~/.dotfiles/.vimrc .vimrc
-ln -s ~/.dotfiles/.zshrc .zshrc
+rm -rf .vim .vimrc .zshrc .config/i3status scripts
+ln -s ~/.dotfiles/.vimrc ~/.vimrc
+ln -s ~/.dotfiles/.zshrc ~/.zshrc
 ln -s ~/.dotfiles/i3status ~/.config/i3status
-ln -s ~/.dotfiles/scripts scripts
+ln -s ~/.dotfiles/scripts ~/scripts
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
