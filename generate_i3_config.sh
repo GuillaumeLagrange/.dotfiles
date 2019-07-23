19ee2c70e5121c0885e9c3328cd224cdb9354e68
Guillaume Lagrange <glagrange@netatmo.com>
2019-07-23 14:12:06 +0200

    Add .Xmodmap

--- generate_i3_config.sh
+++ generate_i3_config.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
+mkdir -p ~/.config ~/.config/i3
 cat i3/config_common i3/config_local > ~/.config/i3/config
--- install.sh
+++ install.sh
@@ -5,5 +5,6 @@ ln -s ~/.dotfiles/.vimrc ~/.vimrc
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
 ln -s ~/.dotfiles/i3status ~/.config/i3status
 ln -s ~/.dotfiles/scripts ~/scripts
+ln -s ~/.dotfiles/.Xmodmap ~/.Xmodmap
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
