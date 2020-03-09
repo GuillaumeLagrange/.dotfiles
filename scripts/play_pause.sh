f10848469a7f483456a7024b44e22b3cae41f8a8
Guillaume Lagrange <guillaume.m.lagrange@gmail.com>
2020-03-09 15:07:04 +0100

    Update MPRIS scripts

--- scripts/play_pause.sh
+++ scripts/play_pause.sh
@@ -1,10 +1,7 @@
 #!/bin/sh
 
 # Get current media player name
-MPRIS_TARGET=`dbus-send --session --dest=org.freedesktop.DBus --type=method_call --print-reply=literal /org/freedesktop/DBus org.freedesktop.DBus.ListNames | tr ' ' '\n' | grep 'org.mpris.MediaPlayer2.mps-youtube' | head -n 1`
-if [ -z $MPRIS_TARGET ]; then
-  MPRIS_TARGET=`dbus-send --session --dest=org.freedesktop.DBus --type=method_call --print-reply=literal /org/freedesktop/DBus org.freedesktop.DBus.ListNames | tr ' ' '\n' | grep 'org.mpris.MediaPlayer2.spotify' | head -n 1`
-fi
+MPRIS_TARGET=`dbus-send --session --dest=org.freedesktop.DBus --type=method_call --print-reply=literal /org/freedesktop/DBus org.freedesktop.DBus.ListNames | tr ' ' '\n' | grep 'org.mpris.MediaPlayer2.Firefox' | head -n 1`
 
 # Play/payse media player
 if [ ! -z "$MPRIS_TARGET" ]; then
